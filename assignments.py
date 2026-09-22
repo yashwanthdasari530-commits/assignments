3f70394b6a3cf63cdb7bf4a86a7eeb93bf714d65
yashwanthdasari530-commits <yashwanthdasari530@gmail.com>
2026-09-22 22:57:04 +0530

    Update assignments.py

---
+++ assignments.py
@@ -0,0 +1,129 @@
+# 1. Printing Messages & Basic print()
+
+print("Hello World!")
+print("Welcome to Python.")
+print("D.Yashwanth")
+print("I am learning Python.")
+print("Python Programming.")
+print("Good Morning.")
+print("Welcome Students.")
+print("Python is easy.")
+print("My First Program.")
+print("Hello from Python!")
+
+
+# 2. Quotes, Messages vs Calculations
+
+print("2 + 5")
+print(2 + 5)
+
+print("10 - 3")
+print(10 - 3)
+
+print("Hello World!")
+print("Python")
+print("Hello")
+print("Hello")
+
+print("10 + 20")
+print(10 + 20)
+
+
+# 3. Variables
+
+name = "Yashwanth"
+age = 20
+marks = 85
+price = 99.5
+
+city = "Chennai"
+print(city)
+
+score = 95
+print(score)
+
+x = 10
+print(x)
+
+message = "Welcome"
+print(message)
+
+a = 10
+b = 20
+print(a + b)
+
+quantity = 5
+price = 10
+print(quantity * price)
+
+
+# 4. Data Types
+
+print(type(25))
+print(type(25.5))
+print(type("25"))
+
+age = 21
+height = 5.8
+name = "Arun"
+
+age = 20
+price = 99.5
+name = "Ravi"
+
+marks = 90
+print(type(marks))
+
+temperature = 36.5
+print(type(temperature))
+
+message = "Welcome"
+print(type(message))
+
+
+# 5. Sequence of Instructions
+
+print("Start")
+print("Middle")
+print("End")
+
+print("First")
+print("Second")
+print("Third")
+
+x = 10
+print(x)
+x = 20
+print(x)
+
+number = 5
+print(number)
+number = 10
+print(number)
+
+print("A")
+print("B")
+print("C")
+
+name = "Ravi"
+age = 20
+print(name)
+print(age)
+
+print(1)
+print(2)
+print(3)
+print(4)
+
+x = 5
+y = 10
+print(x)
+print(y)
+
+print("Start")
+print(2 + 3)
+print("End")
+
+print("Login")
+print("Dashboard")
+print("Logout")
